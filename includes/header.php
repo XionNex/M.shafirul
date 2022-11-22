6d0f6655f69b722555a1f8ffd51fdd4574ddb567
XionNex <109809753+XionNex@users.noreply.github.com>
2022-11-22 18:52:30 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!--header-->
+    <div class="header" id="home">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+          </button>
+          <h1><a class="navbar-brand" href="index.php">MyUni</a></h1>
+          </div>
+              <div class="clearfix"> </div>
+            </div><!-- /.navbar-collapse -->
+        <!-- /.container-fluid -->
+
+      </nav>
+<!--/script-->
+       <div class="clearfix"> </div>
+</div>
+<!-- Top Navigation -->
+
+<!--header-->
